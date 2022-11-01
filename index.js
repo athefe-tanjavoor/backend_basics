f7a47b8e1db46d747c65736e0f524f4ee34b068c
athefe <tanjavoorathefe@gmail.com>
2022-11-01 13:05:27 +0530

    changes

--- index.js
+++ index.js
@@ -11,33 +11,61 @@
 //     console.log("server started")
 // })
 
+if (process.env.NODE_ENV !== "production") require("dotenv").config();
 const express = require("express");
 const app = express();
 const bcrypt = require("bcrypt");
+const passport = require("passport");
+const flash = require("express-flash");
+const session = require("express-session");
+const initializepassport = require("./passport-config");
+initializepassport(passport, (email) =>
+  user.find((user) => user.email === email),
+  (id) =>
+  user.find((user) => user.id === id)
+);
 const user = [];
 
 app.set("view-engine", "ejs");
-app.use(express.urlencoded({extended:false}))
+app.use(express.urlencoded({ extended: false }));
+app.use(flash());
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.get("/", (req, res) => {
-  res.render("index.ejs", { name: "aatif" });
+app.get("/",checkAuthenticated, (req, res) => {
+  res.render("index.ejs", { name:req.user.name  });
 });
 
 app.get("/login", (req, res) => {
   res.render("login.ejs");
 });
+app.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/",
+    failureFlash: true,
+  })
+);
 app.get("/register", (req, res) => {
   res.render("register.ejs");
 });
 
-app.post("/register", async (req, res) => {athefe
-  try { 
-    const hashedPassword = await bcrypt.hash(req.body.password, 10)
+app.post("/register", async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
     user.push({
       id: Date.now().toString(),
       name: req.body.name,
       email: req.body.email,
-      password:hashedPassword
+      password: hashedPassword,
     });
     res.redirect("/login");
   } catch {
@@ -45,6 +73,12 @@ app.post("/register", async (req, res) => {athefe
   }
   console.log(user);
 });
+function checkAuthenticated(req, tres, next) {
+  if (req.isAuthenticated()) {
+    return next()
+  }
+  res.redirect('/login')
+}
 app.listen(4002, () => {
   console.log("server started");
 });
